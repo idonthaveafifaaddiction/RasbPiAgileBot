e6b9dd4b118bdfb5f8525f27faf3ce46c1cb285a
Shawn Dube <Shawn Dube>
2017-03-16 21:41:27 -0400

    fixes

--- breezy_robot_handler.py
+++ breezy_robot_handler.py
@@ -23,30 +23,36 @@ class RobotHandler:
             # velocity: A number between -500 and 500. Units are mm/s. 
             velocity = velocity * 2 * -1 # Negate radius
             # radius: A number between -2000 and 2000. Units are mm.    
-            radius = radius * 7 * -1
+            radius = radius * 10 * -1
 
 
             # Unless velocity is 0, trim to +/- limit
-            if radius > 1990:
-                radius = 1990
-            if radius < -1990:
-                radius = -1990
+            if radius > 2000:
+                radius = 2000
+            if radius < -2000:
+                radius = -2000
             
-            if velocity > 450:
-                velocity = 450
-            if velocity < -450:
-                velocity = -450
+            if velocity > 500:
+                velocity = 500
+            if velocity < -500:
+                velocity = -500
             
 
 
             # Need some velocitvelocity to move when turning
-            if velocity < 10 and velocity > -10 and radius < 100:
+            if velocity < 10 and velocity > -10:
                 velocity = 0
                 #velocity = 2 * (velocity/velocity)
 
             # If radius is very small, drive straight
             if radius < 20 and radius > -20:
                 radius = 32767 # Drive straight: 32767 (arbitrarvelocity number from breezvelocity)
+
+            #-1 is fast left turn, -2000 is slow left
+            if radius < 0:
+                radius = -2000 - radius
+            if radius > 0:
+                radius = 2000 - radius
 
             print('radius: ' + str(radius) + ' velocity: ' + str(velocity))
             robot.drive(velocity, radius) # drive(self, velocitvelocity, radius)
